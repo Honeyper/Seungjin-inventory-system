ecc9ecd6f8ade557159dc9f0106f1f898fab3af9
Honeyper <kang14296@gmail.com>
2026-07-22 11:50:52 +0900

    chore: trigger dev pages deployment

--- frontend/config.dev.js
+++ frontend/config.dev.js
@@ -1,3 +1,4 @@
+// DEV Pages deployment marker: 2026-07-22
 window.SEUNGJIN_CONFIG = {
   ENV: "dev",
   API_URL: "https://script.google.com/macros/s/AKfycbzSz-9IspdGb_wcAIUVhokQdQR0egaiR5M1sJ9PQVX5pjm_w7-FPU3gaj-cmLwjAvxvsg/exec"
